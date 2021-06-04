02a1b84f1abd8301bbcea52faf0c17d5308531c2
Daniel Pineda <dpinedaj@unal.edu.co>
2021-06-03 20:15:50 -0500

    Updated fileMaker to handle exceptions

--- Scripts/fileMaker.py
+++ Scripts/fileMaker.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import shutil
 import time
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from modules.constants import Constants as cts
@@ -33,16 +34,24 @@ if __name__ == "__main__":
     fm = FileMaker()
     originPath = fm.constants.ORIGIN_PATH
     destinyPath = fm.constants.DESTINY_PATH
+    os.makedirs(originPath, exist_ok=True)
+    os.makedirs(destinyPath, exist_ok=True)
+
     n = 1
-    while True:
-        if fm.files.fn_get_len_path(destinyPath) >= fm.constants.ITER:
-            fm.fn_cleaner(destinyPath)
-        else:
-            while fm.files.fn_get_len_path(originPath) < fm.constants.ITER :
-                fm.fn_file_maker(originPath, n)
-                n += 1
+    try:
+        while True:
+            if fm.files.fn_get_len_path(destinyPath) >= fm.constants.ITER:
+                fm.fn_cleaner(destinyPath)
             else:
-                print('There\'s no space')
-                time.sleep(10)
+                while fm.files.fn_get_len_path(originPath) < fm.constants.ITER :
+                    fm.fn_file_maker(originPath, n)
+                    n += 1
+                else:
+                    print('There\'s no space')
+                    time.sleep(10)
+    finally:
+        shutil.rmtree(originPath)
+        shutil.rmtree(destinyPath)
+
 
             
--- modules/constants.py
+++ modules/constants.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
-import sys
 import os
-from pathlib import Path
 
 class Constants:
 
@@ -12,8 +10,8 @@ class Constants:
         self.__ERROR = -1
         self.__NODATA = 100
         self.__GLOBALCONFIG = os.path.join(os.path.dirname(os.path.abspath(__file__)),"config","globalConfig.properties")
-        self.__ORIGIN_PATH = os.path.join(Path(os.getcwd()).parent.absolute(), 'Origin')
-        self.__DESTINY_PATH = os.path.join(Path(os.getcwd()).parent.absolute(), 'Destiny')
+        self.__ORIGIN_PATH = os.path.join(os.getcwd(), 'Origin')
+        self.__DESTINY_PATH = os.path.join(os.getcwd(), 'Destiny')
         self.__ITER = 1000
         self.__DB_URL = 'postgresql://admin:admin@localhost:5432/pruebas'
 
